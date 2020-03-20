577ecdd0ef2e267d28601afc8724260e70e27610
ydefu <ydefu@outlook.com>
2020-03-20 21:32:36 +0800

    重新学习ch13 Page 489

--- ch13/ch13-tabtenn-2/tabtenn.cpp
+++ ch13/ch13-tabtenn-2/tabtenn.cpp
@@ -11,3 +11,25 @@ void TableTennisPlayer::Name() const
 {
     std::cout << lastname << ", " << firstname;
 }
+
+// RatePlayer的方法
+// 使用了列表初始化，TableTennisPlayer(fn, ln, ht)使用了构造函数
+//
+// 创建派生类对象时，程序首先创建基类对象。
+// 这意味着基类对象应当在程序进入派生类对象的构造函数之前被创建。
+// c++使用成员初始化列表来完成这种工作
+//
+// 1. 首先创建基类对象
+// 2. 派生类构造函数应通过成员初始化列表将基类信息传递给基类构造函数
+// 3. 派生类构造函数应初始化派生类新增的数据成员
+RatePlayer::RatePlayer(unsigned int r, const string & fn,
+        const string & ln,
+        bool ht) : TableTennisPlayer(fn, ln, ht)
+{
+    rating = r;
+}
+
+// 使用了列表初始化，TableTennisPlayer(tp)使用了编译器提供的默认复制构造函数
+RatePlayer::RatePlayer(unsigned int r,
+        const TableTennisPlayer &tp) : TableTennisPlayer(tp), rating(r)
+{}
--- ch13/ch13-tabtenn-2/usett.cpp
+++ ch13/ch13-tabtenn-2/usett.cpp
@@ -1,26 +1,41 @@
 #include <iostream>
 #include "tabtenn.h"
 
+// 派生类可以调用基类非私有接口
+// 基类指针可以不进行显示类型转换直接指向派生类对象
+// 基类引用可以不进行显示类型转换直接引用派生类对象
+// 基类指针或引用指向或引用派生类对象时，只能调用基类方法
+
 int main(void)
 {
-    using namespace std;
+    using std::cout;
+    using std::endl;
 
-    TableTennisPlayer player1("Chuck", "Blizzard", true);
-    TableTennisPlayer player2("Tara", "Boomda", false);
+    TableTennisPlayer player1("Tara", "Boomdea", false);
+    RatePlayer rplayer1(1140, "Mallory", "Duck", true);
+
+    rplayer1.Name();
+    if (rplayer1.HasTable()) {
+        cout << ": has a table\n";
+    } else {
+        cout << ": hasn't a table\n";
+    }
 
     player1.Name();
     if (player1.HasTable()) {
-        cout << ": has a table" << endl;
+        cout << ": has a table\n";
     } else {
-        cout << ": hasn't a table" << endl;
+        cout << ": hasn't a table\n";
     }
 
-    player2.Name();
-    if (player2.HasTable()) {
-        cout << ": has a table" << endl;
-    } else {
-        cout << ": hasn't a table" << endl;
-    }
+    cout << "Name: ";
+    rplayer1.Name();
+    cout << "; Rating: " << rplayer1.Rating() << endl;
+
+    RatePlayer rplayer2(1212, player1); // 调用的是第二个构造函数
+    cout << "Name: ";
+    rplayer2.Name();
+    cout << "; Rating: " << rplayer1.Rating() << endl;
 
     return 0;
 }
